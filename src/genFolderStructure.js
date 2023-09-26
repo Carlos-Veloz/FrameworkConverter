bda261d0938b9cedbd1f3f8c3e9718e2212b8bcf
Carlos_Veloz <carlos.veloz@wizeline.com>
2023-09-26 11:59:01 -0600

    fixing issue with no output folder for commands file

--- src/genFolderStructure.js
+++ src/genFolderStructure.js
@@ -8,14 +8,14 @@ const folderStructure = "Given the following folder structure for the legacy tes
   "\n/java\n\t/pages\n\t/tests\n\t/utils\n/resources\n\t/testdata\n\t\tData provider.xlsx\n.gitignore\nREADME.md\npom.xml\ntestng.xml\n " +
   "Could you tell me what are the commands that I have to execute in command line to: 1. Create a new directory for the project, " +
   "2. Initialize a new Node.js project, 3. Install dependencies needed, 4.Create the necessary folders for the new framework structure?"
-
-const file = "newFile.js";
+const outputFolder = "./commands/";
+const file = "commands.txt";
 
 (async () => {
   try {
     let prompt = await createMessages(folderStructure);
     let output = await generate(prompt);
-    await createOutputFile(file, output);
+    await createOutputFile(outputFolder, file, output);
   } catch (error) {
     console.error(error);
   }
